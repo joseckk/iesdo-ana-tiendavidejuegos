7bc7a8703b797da96a95992524df5dda6a001bf9
Jose Carlos De La Rosa Methvin <josecarlos.delarosa@iesdonana.org>
2020-11-11 19:52:33 +0100

    Borrar de la tabla tienda

--- tiendas/borrar.php
+++ tiendas/borrar.php
@@ -1,15 +1,18 @@
 <?php
 
+require '../comunes/auxiliar.php';
+
+
 if (isset($_POST['id'])) {
 
     $id = trim($_POST['id']);
 
-    $pdo = new PDO('pgsql:host=localhost;dbname=bd', 'jose', 'jose');
+    $pdo = conectar();
 
-    $sent = $pdo->prepare("DELETE FROM videojuegos WHERE id = :id");
+    $sent = $pdo->prepare("DELETE FROM tienda WHERE id = :id");
     
     $sent->execute([':id' => $id]);
 }
 
 setcookie('borrar','1');
-header('location: index.php');
+volver();
