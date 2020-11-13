ff59ec5f7c9fefd820adc76c30f9b301d2a23b9b
Jose Carlos De La Rosa Methvin <josecarlos.delarosa@iesdonana.org>
2020-11-13 20:40:18 +0100

    Controlar problema de seguridad CSRF = Cross-Site Request Forgery

--- tiendas/borrar.php
+++ tiendas/borrar.php
@@ -5,6 +5,14 @@ require '../comunes/auxiliar.php';
 
 comprobar_admin();
 
+if (!isset($_POST['csrf_token'])) {
+    volver();
+    return;
+} elseif ($_POST['csrf_token'] != $_SESSION['csrf_token']) {
+    volver();
+    return;
+}
+
 if (isset($_POST['id'])) {
 
     $id = trim($_POST['id']);
