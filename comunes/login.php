3b7978765dfaa50195910694235f17d9e8e0f323
Jose Carlos De La Rosa Methvin <josecarlos.delarosa@iesdonana.org>
2020-11-19 19:49:23 +0100

    empezando a maquetar con Bootstrap 4

--- comunes/login.php
+++ comunes/login.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
     <?php
@@ -46,17 +47,28 @@
         }
     }
     ?>
-    <form action="" method="post">
-        <p>
-            <label for="login">Login:</label>
-            <input type="text" name="login" id="login"
-                   value="<?= hh($login) ?>">
-        </p>
-        <p>
-            <label for="password">Contraseña:</label>
-            <input type="password" name="password" id="password">
-        </p>
-        <button type="submit">Login</button>
-    </form>
+    
+    <div class="container text-center">
+        <form action="" method="post">
+            <div class="row mt-5">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="login">Nombre: </label>
+                        <input type="text" class="form-control" id="login" placeholder="Introduce tu nombre de usuario"
+                                value="<?= hh($login) ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Contraseña</label>
+                        <input type="password" class="form-control" id="password" placeholder="Contraseña">
+                    </div>
+                        <button type="submit" class="btn btn-primary">Login</button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Indice página principal</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
     <?php 
@@ -13,8 +14,40 @@
         comprobar_logueado();
         head();
     ?>
-    <p class="tiendas"><a href="tiendas/index.php">Consultar tiendas</a></p>
-    <p class="videojuegos"><a href="videojuegos/index.php">Consultar videojuegos</a></p>
-    <p class="usuarios"><a href="usuarios/principal.php">Consultar usuarios</a></p>
+
+    <div class="container-fluid">
+        <div class="row-md-12">
+            <div class="col-md-12">
+                <nav class="navbar navbar-expand-lg navbar-light bg-info">
+                    <a class="navbar-brand" href="index.php">Inicio</a>
+                    
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="tiendas/index.php">Tiendas <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="videojuegos/index.php">Videojuegos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="usuarios/principal.php">Usuarios</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+
+
+        <div class="row-md-12 mt-1">
+            <div class="col-md-12 text-center">
+                <img src="/imagenes/fon.jpg" width="100%">
+            </div>
+        </div> 
+    </div>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
