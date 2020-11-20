73df54c6820c61f51ee83a8ec6b5e4c5ba0f775e
Jose Carlos De La Rosa Methvin <josecarlos.delarosa@iesdonana.org>
2020-11-20 20:43:15 +0100

    maquetando el login

--- comunes/login.php
+++ comunes/login.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
     <?php
@@ -46,17 +47,32 @@
         }
     }
     ?>
-    <form action="" method="post">
-        <p>
-            <label for="login">Login:</label>
-            <input type="text" name="login" id="login"
-                   value="<?= hh($login) ?>">
-        </p>
-        <p>
-            <label for="password">Contraseña:</label>
-            <input type="password" name="password" id="password">
-        </p>
-        <button type="submit">Login</button>
-    </form>
+
+    <div class="container">
+        <form action="" method="post">
+            <div class="form-group mt-5 mr-5">
+                <label class="col-lg-4 control-label" for="login" >Nombre de usuario: </label>
+                <div class="col-lg-4">
+                    <input type="text" class="form-control" name="login" id="login" 
+                            value="<?= hh($login) ?>">
+                </div>
+            </div>
+            <div class="form-group mt-5">
+                <label class="col-lg-4 control-label" for="password">Contraseña: </label>
+                <div class="col-lg-4">
+                    <input type="password" class="form-control" name="password" id="password" 
+                            placeholder="introduzca su contraseña">
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <button type="submit" class="btn btn-primary mt-5">login</button>
+            </div>
+        </form>
+    </div>
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
