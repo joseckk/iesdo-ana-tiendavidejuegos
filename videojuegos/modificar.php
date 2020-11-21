3bdb545590bee04ddc934fe3d308b9e66faa4d22
Jose Carlos De La Rosa Methvin <josecarlos.delarosa@iesdonana.org>
2020-11-21 13:53:22 +0100

    maquetando insertar y modificar de videojuegos

--- videojuegos/modificar.php
+++ videojuegos/modificar.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Modificar un videojuego</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
     
@@ -228,69 +229,108 @@
     
     ?>
 
-    <form action="" method="post">
-        <p>
-            <label for="video_tipo">Tipo:</label>
-            <input type="text" name="video_tipo" id="video_tipo" 
-                    value="<?= hh($video_tipo_fmt) ?>">
-        </p>
-        <p>
-            <label for="vnombre">Nombre:</label>
-            <input type="text" name="vnombre" id="vnombre" 
-                    value="<?= hh($vnombre_fmt) ?>">
-        </p>
-        <p>
-            <label for="precio">Precio:</label>
-            <input type="text" name="precio" id="precio" 
-                    value="<?= hh($precio_fmt) ?>">
-        </p>
-        <p>
-            <label for="pegi">Pegi:</label>
-            <input type="text" name="pegi" id="pegi" 
-                    value="<?= hh($pegi_fmt) ?>">
-        </p>
-        <p>
-            <label for="fecha_alt">Fecha de alta:</label>
-            <input type="text" name="fecha_alt" id="fecha_alt" 
-                    value="<?= hh($fecha_alt_fmt) ?>">
-        </p>
-        <p>
-            <label for="fecha_baj">Fecha de baja:</label>
-            <input type="text" name="fecha_baj" id="fecha_baj" 
-                    value="<?= hh($fecha_baj_fmt) ?>">
-        </p>
-        <p>
-            <label for="disponibilidad">Disponibilidad:</label>
-            <select name="disponibilidad" id="disponibilidad">
-                <option value="<?= '' ?>"></option>
-                <option value= true <?= selected($disponibilidad, '1') ?>>stock</option>
-                <option value= false <?= selected($disponibilidad, '0') ?>>sin fecha de entrada</option>
-            </select>
-        </p>
-        <p>
-            <label for="usuario_id">Usuario:</label>
-            <select name="usuario_id" id="usuario_id">
-                <option value="<?= '' ?>"></option>
-                <?php foreach (lista_usuarios($pdo) as $key => $value) :?>
-                    <option value="<?= $key ?>" <?= selected($usuario_id, $key) ?>>
-                        <?= hh($value) ?>
-                    </option>
-                <?php endforeach ?>
-            </select>
-        </p>
-        <p>
-            <label for="tienda_id">Tienda:</label>
-            <select name="tienda_id" id="tienda_id">
-                <option value="<?= '' ?>"></option>
-                <?php foreach (lista_tiendas($pdo) as $key => $value) :?>
-                    <option value="<?= $key ?>" <?= selected($tienda_id, $key) ?>>
-                        <?= hh($value) ?>
-                    </option>
-                <?php endforeach ?>
-            </select>
-        </p>
-        <button type="submit">Modificar</button>
-        <?php cancelar() ?>
-    </form>
+    <div class="container-fluid">
+        <div class="row-md-12">
+            <div class="col-md-12">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                            
+                    <img src="/imagenes/Rubik.jpg" width="5%" height="2%">
+
+                    <a class="navbar-brand ml-5" href="../index.php">Inicio</a>
+
+                </nav>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <form action="" method="post">
+            <div class="form-group mt-5 mr-5">
+                <label class="col-lg-4 control-label" for="video_tipo">Tipo:</label>
+                <div class="col-lg-4">
+                    <input type="text" class="form-control" name="video_tipo" id="video_tipo" 
+                            value="<?= hh($video_tipo_fmt) ?>">
+                </div>
+            </div>
+            <div class="form-group mt-5">
+                <label class="col-lg-4 control-label" for="vnombre">Nombre:</label>
+                <div class="col-lg-4">
+                    <input type="text" class="form-control" name="vnombre" id="vnombre"
+                            value="<?= hh($vnombre_fmt) ?>">
+                </div>
+            </div>
+            <div class="form-group mt-5">
+                <label class="col-lg-4 control-label" for="precio">Precio:</label>
+                <div class="col-lg-4">
+                    <input type="text" class="form-control" name="precio" id="precio"
+                            value="<?= hh($precio_fmt) ?>">
+                </div>
+            </div>
+            <div class="form-group mt-5">
+                <label class="col-lg-4 control-label" for="pegi">Pegi:</label>
+                <div class="col-lg-4">
+                    <input type="text" class="form-control" name="pegi" id="pegi"
+                            value="<?= hh($pegi_fmt) ?>">
+                </div>
+            </div>
+            <div class="form-group mt-5">
+                <label class="col-lg-4 control-label" for="fecha_alt">Fecha de alta:</label>
+                <div class="col-lg-4">
+                    <input type="text" class="form-control" name="fecha_alt" id="fecha_alt"
+                            value="<?= hh($fecha_alt_fmt) ?>">
+                </div>
+            </div>
+            <div class="form-group mt-5">
+                <label class="col-lg-4 control-label" for="fecha_baj">Fecha de baja:</label>
+                <div class="col-lg-4">
+                    <input type="text" class="form-control" name="fecha_baj" id="fecha_baj"
+                            value="<?= hh($fecha_baj_fmt) ?>">
+                </div>
+            </div>
+            <div class="form-group mt-5">
+                <label class="col-lg-4 control-label" for="disponibilidad">Disponibilidad:</label>
+                <div class="col-lg-4">
+                    <select class="form-control" name="disponibilidad" id="disponibilidad">
+                        <option value="<?= '' ?>"></option>
+                        <option value= true <?= selected($disponibilidad, '1') ?>>stock</option>
+                        <option value= false <?= selected($disponibilidad, '0') ?>>sin fecha de entrada</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group mt-5">
+                <label class="col-lg-4 control-label" for="usuario_id">Usuario:</label>
+                <div class="col-lg-4">
+                    <select class="form-control" name="usuario_id" id="usuario_id">
+                        <option value="<?= '' ?>"></option>
+                        <?php foreach (lista_usuarios($pdo) as $key => $value) :?>
+                            <option value="<?= $key ?>" <?= selected($usuario_id, $key) ?>>
+                                <?= hh($value) ?>
+                            </option>
+                        <?php endforeach ?>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group mt-5">
+                <label class="col-lg-4 control-label" for="tienda_id">Tienda:</label>
+                <div class="col-lg-4">
+                    <select class="form-control" name="tienda_id" id="tienda_id">
+                        <option value="<?= '' ?>"></option>
+                        <?php foreach (lista_tiendas($pdo) as $key => $value) :?>
+                            <option value="<?= $key ?>" <?= selected($tienda_id, $key) ?>>
+                                <?= hh($value) ?>
+                            </option>
+                        <?php endforeach ?>
+                    </select>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <button type="submit" class="btn btn-primary mt-5">Modificar</button>
+            </div>
+        </form>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
